6f7dfa903251f45134a753fb344b026375900906
DillDogG <dilldoggaming@gmail.com>
2024-01-09 14:25:56 -0700

    First Creation

---
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAutonomusAgent : AIAgent
+{
+    public AIPerception perception = null;
+
+    private void Update()
+    {
+        // 
+    }
+}
